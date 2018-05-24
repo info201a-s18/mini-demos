1e6e3067387d6d468c4bc4c1c74ca5b34ccea308
andrewk7 <andrewk717@gmail.com>
2018-05-23 23:44:27 -0700

    removing answers

--- webscraper-demo.R
+++ webscraper-demo.R
@@ -27,120 +27,81 @@ rank_data<-as.numeric(rank_data)
 head(rank_data)
 
 #Using CSS selectors to scrape the title section
-title_data_html <- html_nodes(webpage,'.lister-item-header a')
 
 #html to text 
-title_data <- html_text(title_data_html)
 
 #look at data
-head(title_data)
 
 #Using CSS selectors to scrape the description section
-description_data_html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
 
 #Converting the description data to text
-description_data <- html_text(description_data_html)
 
 #look at data
-head(description_data)
 
 #Data-Preprocessing: removing '\n'
-description_data<-gsub("\n","",description_data)
 
 #Using CSS selectors to scrap the Movie runtime section
 
-runtime_data_html <- html_nodes(webpage,'.text-muted .runtime')
-
 #Converting the movie runtime data to text
-runtime_data <- html_text(runtime_data_html)
 
 #Let's have a look at the movie runtime
-head(runtime_data)
 
 #Data-Preprocessing: removing mins and converting it to numerical
 
-runtime_data<-gsub(" min","",runtime_data)
-runtime_data<-as.numeric(runtime_data)
 
 #Let's have another look at the runtime data
-head(rank_data)
 
-genre_data_html <- html_nodes(webpage,'.genre')
 
 #Converting the genre data to text
-genre_data <- html_text(genre_data_html)
 
 #Let's have a look at the genre
-head(genre_data)
 
 #Data-Preprocessing: removing \n
-genre_data<-gsub("\n","",genre_data)
 
 #Data-Preprocessing: removing excess spaces
-genre_data<-gsub(" ","",genre_data)
 
 #taking only the first genre of each movie
-genre_data<-gsub(",.*","",genre_data)
 
 #Convering each genre from text to factor
-genre_data<-as.factor(genre_data)
 
 #Let's have another look at the genre data
-head(genre_data)
 
 #Using CSS selectors to scrap the IMDB rating section
-rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
 
 #Converting the ratings data to text
-rating_data <- html_text(rating_data_html)
 
 #Let's have a look at the ratings
-head(rating_data)
 
 #Data-Preprocessing: converting ratings to numerical
-rating_data<-as.numeric(rating_data)
 
 #Let's have another look at the ratings data
-head(rating_data)
 
 
 #Using CSS selectors to scrap the directors section
-directors_data_html <- html_nodes(webpage,'.text-muted+ p a:nth-child(1)')
 
 #Converting the directors data to text
-directors_data <- html_text(directors_data_html)
 
 #Let's have a look at the directors data
-head(directors_data)
 
 #Data-Preprocessing: converting directors data into factors
-directors_data<-as.factor(directors_data)
 
 #Using CSS selectors to scrap the actors section
-actors_data_html <- html_nodes(webpage,'.lister-item-content .ghost+ a')
 
 #Converting the gross actors data to text
-actors_data <- html_text(actors_data_html)
 
 #Let's have a look at the actors data
-head(actors_data)
 
 #Data-Preprocessing: converting actors data into factors
-actors_data<-as.factor(actors_data)
+
 #Using CSS selectors to scrap the gross revenue section
-gross_data_html <- html_nodes(webpage,'.ghost~ .text-muted+ span')
 
 #Converting the gross revenue data to text
-gross_data <- html_text(gross_data_html)
 
 #Let's have a look at the votes data
-head(gross_data)
 
 
 #Data-Preprocessing: removing '$' and 'M' signs
-gross_data<-gsub("M","",gross_data)
 
-gross_data<-substring(gross_data,2,6)
 
 #Let's check the length of gross data
 length(gross_data)
@@ -148,40 +109,13 @@ length(gross_data)
 
 
 #Filling missing entries with NA
-for (i in c(73, 74,76,77,80,87,88,89)){
-  
-  a<-gross_data[1:(i-1)]
-  
-  b<-gross_data[i:length(gross_data)]
-  
-  gross_data<-append(a,list("NA"))
-  
-  gross_data<-append(gross_data,b)
-  
-}
+
 
 #Data-Preprocessing: converting gross to numerical
-gross_data<-as.numeric(gross_data)
 
 #Let's have another look at the length of gross data
-length(gross_data)
-
-summary(gross_data)
 
 
-movies_df<-data.frame(Rank = rank_data, Title = title_data,
-                      
-                      Description = description_data, Runtime = runtime_data,
-                      
-                      Genre = genre_data, Rating = rating_data, Gross_Earning_in_Mil = gross_data,
-                  
-        
-                      Director = directors_data, Actor = actors_data)
+#library('ggplot2')
 
-library('ggplot2')
-
-qplot(data = movies_df,Runtime,fill = Genre,bins = 30)
-
-ggplot(movies_df,aes(x=Runtime,y=Gross_Earning_in_Mil))+
-  geom_point(aes(size=Rating,col=Genre))
-
+# let's draw some plots!
